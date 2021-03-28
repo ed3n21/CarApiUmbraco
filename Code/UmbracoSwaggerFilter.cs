f9db40653e00c28fbdcba719136d6e6467fc281a
Max <lyuban.maks@gmail.com>
2021-03-28 22:24:53 +0300

    Add swagger

---
+++ Code/UmbracoSwaggerFilter.cs
@@ -0,0 +1,21 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace CarApi.Swagger
+{
+    public class UmbracoSwaggerFilter : IDocumentFilter
+    {
+        // Prevents from displaying uncustom umbraco APIs in Swagger UI
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths = swaggerDoc
+                .paths
+                .Where(x => x.Key.StartsWith("/api"))
+                .ToDictionary(e => e.Key, e => e.Value);
+        }
+    }
+}
